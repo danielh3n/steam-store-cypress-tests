c32c52f03c3a63ba9e47054fe3f2d8f9560918fa
Daniel <danielhentges@gmail.com>
2025-07-03 12:03:40 -0300

    feat: fix search test

--- cypress/e2e/search.cy.js
+++ cypress/e2e/search.cy.js
@@ -1,7 +1,7 @@
 describe('Steam Store - Game Search', () => {
   it('should return relevant results for CS2', () => {
     cy.visit('/')
-    cy.searchGame('cs2')
+    cy.get('#store_nav_search_term', { timeout: 10000 }).should('be.visible').type('cs2{enter}')
     cy.get('.search_result_row').should('contain.text', 'Counter-Strike 2')
   })
 })
